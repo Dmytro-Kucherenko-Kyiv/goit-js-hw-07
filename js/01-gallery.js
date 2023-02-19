4a6288d6fcd32acc1821a14f6f26fb0a70091f84
Dmytro Kucherenko <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-02-19 20:39:17 +0200

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,7 +6,22 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery')
 
-const preview = galleryItems.map((item) => item.preview).join(' ');
+const imgElements = galleryItems.map((item) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}" target="_self" rel="noreferrer noopener">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+}).join('');
+
+gallery.insertAdjacentHTML('beforeend', imgElements)
+
+/* const preview = galleryItems.map((item) => item.preview).join(' ');
 const original = galleryItems.map((item) => item.original).join(' ');
 const description = galleryItems.map((item) => item.description).join(' ');
 
@@ -22,10 +37,34 @@ galleryItems.forEach(image => {
   </a>
 </div>`;
   gallery.innerHTML += item;
-})
+}) */
 
 // 2. Реалізація делегування на div.gallery і отримання url великого зображення.
+
+gallery.addEventListener('click', (event) => 
+  
+  if (event.target !== event.currentTarget)
+  return;
+
+  console.log(event.currentTarget.href)
+    
+)
+
 // 3. Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. 
+
+/* import * as basicLightbox from 'basiclightbox'
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`)
+
+instance.show()
+ */
 //    Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані(.min) файли бібліотеки.
 // 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
 // 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. 
